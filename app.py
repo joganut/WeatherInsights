da9f2a69e1c7bbe8d5b7e8fa8598f560db677a46
joganut <39301354+joganut@users.noreply.github.com>
2024-09-04 14:08:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def generate_recommendations(df):
     recommendations = []  # Initialize the recommendations list
     # URL of the release asset
     asset_url = "https://github.com/joganut/WeatherInsights/releases/download/test/orca-mini-3b-gguf2-q4_0.gguf"
-    local_file_name = "orca-mini-3b.ggmlv3.q4_0.bin"
+    local_file_name = "orca-mini-3b.ggmlv3.q4_0.gguf"
 
     # Download the model file
     response = requests.get(asset_url)
