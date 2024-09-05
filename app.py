3642d02d71ab61e5953fb3510c72a63c6df3a367
joganut <39301354+joganut@users.noreply.github.com>
2024-09-05 01:32:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,6 @@ if location:
         with st.spinner('Generating A.I Recommendations...'):
             recommendations = generate_recommendations(df_current_day, replicate_model)
 
-        for i, rec in enumerate(recommendations):
+        # for i, rec in enumerate(recommendations):
             st.subheader(f"🧠 A.I Recommendations for Today")
             st.markdown(rec)
